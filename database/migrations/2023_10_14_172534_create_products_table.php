aa43a2b3a6c34ac41de98463092aaf87c1df4447
Marianne <marianne.garridom@gmail.com>
2023-10-15 20:45:58 -0600

    migraciones

--- database/migrations/2023_10_14_172534_create_products_table.php
+++ database/migrations/2023_10_14_172534_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,15 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('quantity')->unsigned;
+            $table->string('status')->default(Product::PRODUCTO_NO_DISPONIBLE);
+            $table->string('image');
+            $table->integer('seller_id')->unsigned;
             $table->timestamps();
+
+            $table->foreign('seller_id')->references('id')->on('users');
         });
     }
 
