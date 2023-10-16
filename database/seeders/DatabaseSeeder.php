b4644c6ed66bdbeb31fd6fd5b670eec296859f0d
Marianne <marianne.garridom@gmail.com>
2023-10-16 09:35:56 -0600

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,14 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Buyer;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,12 +18,24 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+    {   
+        DB::statement('SET FOREING_KEY_CHEKS = 0');
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate();
+
+        User::factory(200)->create();
+        Category::factory(30)->create();
+        Product::factory(1000)->create()->each(
+            function($producto){
+            $categorias = Category::all()->random(mt_rand(1,5))->pluck('id');
+
+            $producto -> categories() -> attach($categorias);
+            }
+        );
+        Transaction::factory(1000)->create();
     }
 }
