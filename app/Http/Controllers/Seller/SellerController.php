7dfffbd0c5f413d99f229bce4d7638eb973d43f2
Marianne <marianne.garridom@gmail.com>
2023-10-16 19:54:22 -0600

    get y set de user

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
+use App\Models\Seller;
 use Illuminate\Http\Request;
 
 class SellerController extends Controller
@@ -12,54 +13,18 @@ class SellerController extends Controller
      */
     public function index()
     {
-        //
+
+        $vendedores = Seller::has('products')->get();
+        return response()->json(['data' => $vendedores, 200]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $vendedores = Seller::has('products')->findOrFail($id);
+        return response()->json(['data' => $vendedores, 200]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
@@ -66,5 +67,21 @@ class User extends Authenticatable
 
     public static function generarVerificationToken(){
         return Str::random(40);
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => ucwords($value),
+            set: fn (string $value) => strtolower($value),
+        );
+    }
+
+    protected function email(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => $value,
+            set: fn (string $value) => strtolower($value),
+        );
     }
 }
