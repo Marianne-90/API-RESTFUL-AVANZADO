c305de9d48acf42efdaa6383f5c95b61e26b9395
Marianne <marianne.garridom@gmail.com>
2023-10-26 15:13:26 -0600

    Product -> Categories

---
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Product $product)
+    {
+        $categories = $product->categories;
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Category\CategoryProductController;
 use App\Http\Controllers\Category\CategorySellerController;
 use App\Http\Controllers\Category\CategoryTransactionController;
 use App\Http\Controllers\Product\ProductBuyerController;
+use App\Http\Controllers\Product\ProductCategoryController;
 use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\Product\ProductTransactionController;
 use App\Http\Controllers\Seller\SellerBuyerController;
@@ -53,7 +54,7 @@ Route::resource('categories.buyers', CategoryBuyerController::class)->only(['ind
 Route::resource('products', ProductController::class)->only(['index', 'show']);
 Route::resource('products.transactions', ProductTransactionController::class)->only(['index']);
 Route::resource('products.buyers', ProductBuyerController::class)->only(['index']);
-
+Route::resource('products.categories', ProductCategoryController::class)->only(['index', 'edit', 'destroy']);
 
 /** TRANSACTIONS */
 Route::resource('transactions', TransactionController::class)->only(['index', 'show']);
@@ -65,7 +66,7 @@ Route::resource('sellers', SellerController::class)->only(['index', 'show']);
 Route::resource('sellers.transactions', SellerTransactionController::class)->only(['index']);
 Route::resource('sellers.categories', SellerCategoryController::class)->only(['index']);
 Route::resource('sellers.buyers', SellerBuyerController::class)->only(['index']);
-Route::resource('sellers.products', SellerProductController::class)->except(['create','show', 'edit']);
+Route::resource('sellers.products', SellerProductController::class)->except(['create', 'show', 'edit']);
 
 /** USERS */
 Route::resource('users', UserController::class)->except(['create', 'edit']);
