780a6ac5b47c31991054d3328a56d3fef35445bb
Marianne <marianne.garridom@gmail.com>
2023-10-25 21:26:20 -0600

    SellerProduct controller

---
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Seller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class SellerProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Seller $seller)
+    {
+        $products = $seller->products;
+
+        return $this->showAll($products);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, User $seller)
+    {
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'image' => 'required|image',
+        ];
+
+        $request->validate($rules);
+
+        $data = $request->all();
+
+        $data['status'] = Product::PRODUCTO_NO_DISPONIBLE;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create($data);
+
+        return $this->showOne($product, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Seller $seller, Product $product)
+    {
+        $rules = [
+            'quantity' => 'integer|min:1',
+            'image' => 'image',
+            'status' => 'in ' . Product::PRODUCTO_NO_DISPONIBLE . ',' . Product::PRODUCTO_DISPONIBLE,
+        ];
+
+        $request->validate($rules);
+
+        $this->verificarVendedor($seller, $product);
+
+
+        $product->fill($request->only([
+            'name',
+            'description',
+            'quantity'
+        ]));
+
+        if ($request->has('status')) {
+            $product->status = $request->status;
+
+            if ($product->estaDisponible() && $product->categories()->count() == 0) {
+                return $this->errorResponse('Un producto actuvo debe tener al menos una categoría', 409);
+            }
+        }
+
+        if ($product->isClean()) {
+            return $this->errorResponse('Se debe especificar al menos un valor para actualizar', 422);
+        }
+
+        $product->save();
+        return $this->showOne($product, 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Seller $seller, Product $product)
+    {
+        $this->verificarVendedor($seller, $product);
+        $product->delete();
+        return $this->showOne($product, 201);
+    }
+
+    protected function verificarVendedor(Seller $seller, Product $product)
+    {
+        if ($seller->id != $product->seller_id) {
+            throw new HttpException(422, "El vendedor no cuenta con las credenciales para editar este producto");
+        }
+    }
+}
