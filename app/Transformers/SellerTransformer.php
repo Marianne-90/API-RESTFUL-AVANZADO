de9b577e9622c8ed89b1cd9b3aa4ca9c3723dfce
Marianne <marianne.garridom@gmail.com>
2023-11-06 17:55:30 -0600

    eliminé métodos sin usar en los transformers

--- app/Transformers/SellerTransformer.php
+++ app/Transformers/SellerTransformer.php
@@ -7,23 +7,7 @@ use League\Fractal\TransformerAbstract;
 
 class SellerTransformer extends TransformerAbstract
 {
-    /**
-     * List of resources to automatically include
-     *
-     * @var array
-     */
-    protected array $defaultIncludes = [
-        //
-    ];
-    
-    /**
-     * List of resources possible to include
-     *
-     * @var array
-     */
-    protected array $availableIncludes = [
-        //
-    ];
+
     
     /**
      * A Fractal transformer.
--- app/Transformers/TransactionTransformer.php
+++ app/Transformers/TransactionTransformer.php
@@ -7,24 +7,7 @@ use League\Fractal\TransformerAbstract;
 
 class TransactionTransformer extends TransformerAbstract
 {
-    /**
-     * List of resources to automatically include
-     *
-     * @var array
-     */
-    protected array $defaultIncludes = [
-        //
-    ];
-    
-    /**
-     * List of resources possible to include
-     *
-     * @var array
-     */
-    protected array $availableIncludes = [
-        //
-    ];
-    
+
     /**
      * A Fractal transformer.
      *
